0267b087c1f0dfe51d03badd9569a3b17bbc5083
danya <serdaniel596@gmail.com>
2023-08-19 14:55:12 +0300

    Update Client.java and RegisterDTO.java

--- src/main/java/com/example/appregister/entity/Client.java
+++ src/main/java/com/example/appregister/entity/Client.java
@@ -5,11 +5,6 @@ import lombok.*;
 
 @Entity
 @Table(name = "client")
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-@ToString
 public class Client {
 
     @Id
@@ -29,4 +24,65 @@ public class Client {
     @Column(name = "password")
     private String password;
 
+    public Client() {
+    }
+
+    public Client(Long id, String email, String firstname, String lastname, String password) {
+        this.id = id;
+        this.email = email;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/example/appregister/entity/RegisterDTO.java
+++ src/main/java/com/example/appregister/entity/RegisterDTO.java
@@ -3,14 +3,62 @@ package com.example.appregister.entity;
 import lombok.*;
 
 @Data
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @ToString
 public class RegisterDTO {
     private String email;
     private String firstname;
     private String lastname;
     private String password;
+
+    public RegisterDTO(String email, String firstname, String lastname, String password) {
+        this.email = email;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.password = password;
+    }
+
+    public RegisterDTO() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "RegisterDTO{" +
+                "email='" + email + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
